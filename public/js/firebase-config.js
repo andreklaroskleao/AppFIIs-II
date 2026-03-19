9ff3a7e76622e570b903df82484f6f5a595b6fe3
andreklaroskleao <andre.klar.leao@gmail.com>
2026-03-19 12:53:53 -0300

    Update and rename firebase-config.js.txt to firebase-config.js

--- public/js/firebase-config.js
+++ public/js/firebase-config.js
@@ -3,12 +3,13 @@ import { getAuth, GoogleAuthProvider } from 'https://www.gstatic.com/firebasejs/
 import { getFirestore } from 'https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js';
 
 const firebaseConfig = {
-  apiKey: 'SUA_API_KEY',
-  authDomain: 'SEU_PROJETO.firebaseapp.com',
-  projectId: 'SEU_PROJETO',
-  storageBucket: 'SEU_PROJETO.appspot.com',
-  messagingSenderId: 'SEU_MESSAGING_SENDER_ID',
-  appId: 'SEU_APP_ID'
+  apiKey: "AIzaSyD4Yq5QBh84yox8-yLS6mxqP3o0vpHQUb4",
+  authDomain: "appfiis-ii.firebaseapp.com",
+  projectId: "appfiis-ii",
+  storageBucket: "appfiis-ii.firebasestorage.app",
+  messagingSenderId: "735639659271",
+  appId: "1:735639659271:web:14174f2a9acb9eefd95ba2",
+  measurementId: "G-DC17FH54CE"
 };
 
 const app = initializeApp(firebaseConfig);
